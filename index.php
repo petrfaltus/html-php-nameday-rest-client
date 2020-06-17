030778a1ac616eb64f27e1cc67b1ec1d75488424
Petr Faltus <petrfaltus@petrfaltus.net>
2020-06-17 19:01:32 +0200

    Create simple main HTML page

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+<meta charset='utf-8'>
+<title>Name day REST client :: Petr Faltus</title>
+<meta name='author' content='Petr Faltus'>
+</head>
+<body>
+<h1>Name day REST client</h1>
+<form>
+<input type='text' name='query' value='' size='40' maxlength='50'>
+<input type='button' value='Search'>
+<p>
+<textarea name='result' cols='55' rows='45' readonly='readonly' disabled='disabled'></textarea>
+</form>
+</body>
+</html>
