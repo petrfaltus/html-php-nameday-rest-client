1ae7e0b60f046aaf2a3984a57c238d5ab010d75d
Petr Faltus <petrfaltus@petrfaltus.net>
2020-06-22 10:02:39 +0200

    Implement the JSON encoding and decoding for REST requests and finish the searching logic

---
+++ .class/tJson.php
@@ -0,0 +1,90 @@
+<?php
+
+class tJson
+{
+  const METHOD_NUMBER = "method_number";
+  const QUERY = "query";
+  const COUNTRY = "country";
+
+  const ERROR_CODE = "error_code";
+  const ERROR_STRING = "error_string";
+  const DATA = "data";
+
+  const METHOD_COUNTRIES_NUMBER = 1;
+  const METHOD_ONE_COUNTRY_NUMBER = 2;
+
+  //----------------------------------------------------------------------------
+  public static function codeQueryCountries()
+  {
+    $output[self::METHOD_NUMBER] = self::METHOD_COUNTRIES_NUMBER;
+
+    $outputJson = json_encode($output);
+
+    return $outputJson;
+  }
+  //----------------------------------------------------------------------------
+  public static function decodeResultCountries(&$inputJson)
+  {
+    $input = json_decode($inputJson, true);
+
+    if ((isset($input[self::ERROR_CODE])) and ($input[self::ERROR_CODE] === 0))
+      if ((isset($input[self::DATA])) and (is_array($input[self::DATA])))
+      {
+        return $input[self::DATA];
+      }
+
+    return null;
+  }
+  //----------------------------------------------------------------------------
+  public static function codeQueryOneCountry(&$query, &$countryCode)
+  {
+    $output[self::METHOD_NUMBER] = self::METHOD_ONE_COUNTRY_NUMBER;
+    $output[self::QUERY] = $query;
+    $output[self::COUNTRY] = $countryCode;
+
+    $outputJson = json_encode($output);
+
+    return $outputJson;
+  }
+  //----------------------------------------------------------------------------
+  public static function decodeResultOneCountry(&$inputJson, &$countryName)
+  {
+    $retString = $countryName." ... ";
+
+    $input = json_decode($inputJson, true);
+
+    if ((!isset($input[self::ERROR_CODE])) or (!isset($input[self::ERROR_STRING])))
+	{
+      $retString .= "chyba parsování";
+      $retString .= PHP_EOL;
+    }
+    else if ($input[self::ERROR_CODE] !== 0)
+    {
+      $retString .= $input[self::ERROR_STRING];
+      $retString .= PHP_EOL;
+    }
+    else if ((!isset($input[self::DATA])) or (!is_array($input[self::DATA])))
+    {
+      $retString .= "data nenalezena";
+      $retString .= PHP_EOL;
+    }
+    else
+    {
+      $retString .= PHP_EOL;
+
+      foreach ($input[self::DATA] as $dataLine)
+      {
+        $retString .= "- ";
+        $retString .= $dataLine;
+        $retString .= PHP_EOL;
+      }
+    }
+
+    $retString .= PHP_EOL;
+
+    return $retString;
+  }
+  //----------------------------------------------------------------------------
+}
+
+?>
--- .class/tSearch.php
+++ .class/tSearch.php
@@ -5,18 +5,41 @@ class tSearch
   protected static $lastError = null;
 
   //----------------------------------------------------------------------------
-  public static function run($query)
+  public static function run(&$query)
   {
     self::$lastError = null; // await no errors
 
-    $output = tWeb::request($query);
-    if ($output == null)
+    $requestJsonCountries = tJson::codeQueryCountries();
+    $replyJsonCountries = tWeb::request($requestJsonCountries);
+    if ($replyJsonCountries == null)
     {
       self::$lastError = "The web request to the REST service failed";
       return null;
     }
 
-    return $output;
+    $countries = tJson::decodeResultCountries($replyJsonCountries);
+    if ($countries == null)
+    {
+      self::$lastError = "The web reply JSON decoding failed";
+      return null;
+    }
+
+    $result = ""; // default value
+    foreach ($countries as $countryCode => $countryName)
+    {
+      $requestJsonOneCountry = tJson::codeQueryOneCountry($query, $countryCode);
+
+      $replyJsonOneCountry = tWeb::request($requestJsonOneCountry);
+      if ($replyJsonOneCountry == null)
+      {
+        self::$lastError = "The web request to the REST service failed";
+        return null;
+      }
+
+      $result .= tJson::decodeResultOneCountry($replyJsonOneCountry, $countryName);
+    }
+
+    return $result;
   }
   //----------------------------------------------------------------------------
   public static function getLastError()
